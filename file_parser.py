2871ebcafb6d117506bc50fa6805307cff3170bd
Alen1ka <57345786+Alen1ka@users.noreply.github.com>
2020-09-26 23:23:10 +0700

    fixed errors related to connection and file names
    else add comments

--- file_parser.py
+++ file_parser.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 
 n = 0
@@ -40,68 +41,91 @@ while n < 100:
         # указываем основные директивы для управления кэшированием
         'Cache-Control': 'no-cache'
     }
-    response = requests.get(url, headers=headers)
+    try:
+        response = requests.get(url, headers=headers)
 
-    # прогоняем документ через bs4, это дает нам объект bs4
-    # он представляет собой документ в виде вложенной структуры "html.parser"
-    soup = BeautifulSoup(response.text, 'html.parser')
+        # прогоняем документ через bs4, это дает нам объект bs4, который
+        # представляет собой документ в виде вложенной структуры "html.parser"
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # с помощью функции поиска bs4 находим
-    # и берем нужные теги div в ходе страницы запроса и в них находим теги a
-    for div in soup.find_all("div", class_="r"):
-        for a in div.find_all("a"):
-            # если тег не содержит class h3 берем ссылку
-            z = a.find('h3')
-            if z is not None:
-                href = a.get('href')
+        # с помощью функции поиска bs4 находим и берем нужные теги div в коде
+        # страницы запроса и в них находим теги a
+        for div in soup.find_all("div", class_="r"):
+            for a in div.find_all("a"):
+                # если тег не содержит class h3 берем ссылку
+                z = a.find('h3')
+                if z is not None:
+                    href = a.get('href')
 
-                # объявляем массив для записи правильных букв
-                print_letters = []
-                # для вывода ссылки только один раз объявим переменную output
-                output = 0
-                url = []
+                    # объявляем массив для записи правильных букв
+                    print_letters = []
+                    # для вывода ссылки один раз объявим переменную output
+                    output = 0
+                    url = []
 
-                # для вывода правильной ссылки
-                # идем по буквам и удаляем всё что есть после &
-                for letter in href:
-                    if letter == '?' and output != 1:
-                        output = +1
-                    if output != 1:
-                        print_letters.append(letter)
-                url = ''.join(print_letters)  # делаем строку из списка
+                    # для вывода правильной ссылки
+                    # идем по буквам и удаляем всё что есть после &
+                    for letter in href:
+                        if letter == '?' and output != 1:
+                            output = +1
+                        if output != 1:
+                            print_letters.append(letter)
+                    url = ''.join(print_letters)  # делаем строку из списка
 
-                # изменим headers
-                headers.update({'Accept-Encoding': 'gzip, deflate, br'})
-                # отправим get-запрос на сайт и сохраним ответ в переменную
-                response = requests.get(url, headers=headers, timeout=15)
+                    # изменим headers
+                    headers.update({'Accept-Encoding': 'gzip, deflate, br'})
+                    # отправим get-запрос на сайт, сохраним ответ в переменную
+                    response = requests.get(url, timeout=25, headers=headers)
 
-                # если в заголовках, есть mime-тип который нужен
-                # берем название файла
-                # для этого находим последний слэш и место где он находится
-                # берем все что есть после него
-                if response.headers['Content-Type'].find(doc) != -1:
-                    if url.find("%") == -1:
-                        find_symbol = '/'
-                        number = url.rfind(find_symbol)
-                        title = url[number:]
-                        title = 'files' + title
-                        print(title)
-                    else:
-                        for spans in a.find_all("span", class_="eipWBe"):
-                            for span in spans.find_all("span"):
-                                # удаляем из начала ссылки часть "/url?q="
-                                # deleted_start = span[7:]
-                                name = " ".join(span.contents)
-                                find_symbol = '›'
-                                number = name.rfind(find_symbol)
-                                deleted_start = name[number + 2:]
-                                title = deleted_start[0:]
-                                title = 'files/' + title + ".docx"
-                                print(title)
-                    # скачиваем файл
-                    with open(title, 'wb') as f:
-                        f.write(response.content)
-                    f.close()
+                    # если в заголовках, есть Content-Type
+                    # и mime-тип который нужен берем название файла
+                    # для этого находим последний слэш и место где он находится
+                    # берем все что есть после него
+                    if response.headers.get('Content-Type') is not None:
+                        if response.headers['Content-Type'].find(doc) != -1:
+                            # если в названии ссылки нет ...
+                            if url.find("...") == -1:
+                                for spa in a.find_all("span", class_="eipWBe"):
+                                    for span in spa.find_all("span"):
+                                        name = " ".join(span.contents)
+                                        find_symbol = '›'
+                                        number = name.rfind(find_symbol)
+                                        # удаляем из начала ссылки часть
+                                        deleted_start = name[number + 2:]
+                                        title = deleted_start[0:]
+                                        if title.find('/'):
+                                            title = title.replace('/', '.')
+                                        title = 'files/' + title
+                                        # добавляем в конец файла расширение
+                                        # если расширения нету
+                                        if title.find('.docx') == -1:
+                                            title = title + '.docx'
+
+                                        # скачиваем файл
+                                        with open(title, 'wb') as f:
+                                            f.write(response.content)
+                                        f.close()
+                            else:
+                                find_symbol = '/'
+                                number = url.rfind(find_symbol)
+                                title = url[number:]
+                                title = 'files' + title
+                                # добавляем в конец файла расширение
+                                # если расширения нету
+                                if title.find('.docx') == -1:
+                                    title = title + '.docx'
+                                
+                                # скачиваем файл
+                                with open(title, 'wb') as f:
+                                    f.write(response.content)
+                                f.close()
+
+    except requests.exceptions.ReadTimeout:
+        # переподключение к серверу
+        time.sleep(3)
+    except requests.exceptions.ConnectionError:
+        # переподключение к серверу
+        time.sleep(3)
 
     # добавим 10 чтобы перейти на следующую страницу
     n += 10
